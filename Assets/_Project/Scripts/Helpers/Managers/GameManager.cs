4549e0d9a721e2ef9f7636819da1b6dac2659aa3
Ariel Ledezma <arielledezma96@gmail.com>
2026-06-11 00:19:18 -0300

    Se agrega boton reinicio

--- Assets/_Project/Scripts/Helpers/Managers/GameManager.cs
+++ Assets/_Project/Scripts/Helpers/Managers/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum GameState
 {
@@ -11,8 +12,23 @@ public class GameManager : MonoBehaviour
 {
     public static GameState CurrentState { get; private set; } = GameState.Playing;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            RestartGame();
+    }
+
     public static void ChangeState(GameState newState)
     {
         CurrentState = newState;
+    }
+
+    public static void RestartGame()
+    {
+        Time.timeScale = 1f;
+        CurrentState = GameState.Playing;
+
+        Scene activeScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(activeScene.name);
     }
 }
