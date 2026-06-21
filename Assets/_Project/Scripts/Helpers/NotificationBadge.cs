68f61440a1388aaad9c21e379f530896ffc09bab
tmaciel96 <tmaciel96@gmail.com>
2026-06-21 19:25:22 -0300

    Resaltado del button de rescate
    Ahora mi boton de rescate se mueve cuando hay >1 pendiente de rescate

--- Assets/_Project/Scripts/Helpers/NotificationBadge.cs
+++ Assets/_Project/Scripts/Helpers/NotificationBadge.cs
@@ -5,17 +5,22 @@ public class NotificationBadge : MonoBehaviour
 {
     [SerializeField] private GameObject badgeContainer;
     [SerializeField] private TextMeshProUGUI countText;
+    [SerializeField] private RescueButtonWobble buttonWobble;
 
     public void UpdateBadge(int count)
     {
         if (count <= 0)
         {
             badgeContainer.SetActive(false);
+            buttonWobble?.StopWobble();
+
         }
         else
         {
             badgeContainer.SetActive(true);
             countText.text = count.ToString();
+            buttonWobble?.StartWobble();
+
         }
     }
 }
